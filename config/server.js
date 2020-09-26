316041dcbde1cb16731ba9fde753c5fa16432bda
Edwardffigueroa <edwardfigueroavelasco@gmail.com>
2020-09-25 21:14:00 -0500

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '117b330d2983f7206b497ba68ae1ae55'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7380fa59-41e0-41be-86fd-7aada2a94552'
+};
